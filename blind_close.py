450050f13e1e81373cff1f43dda3b0d1e892f7a1
whquddnr97 <59822853+whquddnr97@users.noreply.github.com>
2021-06-15 19:58:28 +0900

    change motor running time

--- blind_close.py
+++ blind_close.py
@@ -3,6 +3,14 @@ import time
 
 motor = Motor(forward = 16, backward = 21)
 
+start_time = time.time()
+while(True):
+    motor.backward()
+    if (time.time() - start_time == 10):
+        break
+    else:
+        continue
+    
 motor.backward()
 time.sleep(2)
 
