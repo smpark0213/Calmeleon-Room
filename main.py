f1e7d443cd62fa5d03af288eaa4afd5b2fc17915
Park seung-min <60349584+smpark0213@users.noreply.github.com>
2021-06-15 16:46:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,77 @@ from datetime import datetime
  
 now = datetime.now()
 
+def firebase_init():
+    import firebase_admin
+    from firebase_admin import credentials
+    from firebase_admin import db
+
+    credential = credentials.Certificate('sensorandnetwork-firebase-adminsdk-iy4wj-3e52cbe6c1.json')
+    firebase_admin.initialize_app(credential, {'databaseURL' : 'https://sensorandnetwork-default-rtdb.firebaseio.com/'})
+    dir = db.reference()
+
+    return dir
+
+# DB에서 사용자의 요청을 읽어옴
+# 아무 요청이 없을 시 비어잇는 문자열 반환
+# status가 false일시 메세지 반환
+def get_request(dir):
+    request = dir.child('message').get()
+    request_message = request['message']
+    request_status = request['status']
+
+    if request_status == 'False':
+      return request_message
+    else:
+      return 'Yet'
+# record에 날짜, 메시지, 안밖 온도 저장
+def save_record(dir, request_message, color, temp_in, temp_out):
+    from datetime import datetime
+
+    record_ref = dir.child('record')
+
+    date = datetime.today().strftime('%Y%m%d%H%M')
+
+    record = record_ref.push({'date' : date,
+                            'message' : request_message,
+                            'temperature_inside' : temp_in,
+                            'temperature_outside' : temp_out})
+  
+    decorate_ref = dir.child('Decorate')
+
+    date = datetime.today().strftime('%Y%m%d')
+    decorate = decorate_ref.push({'color' : color,
+                                'date' : date})
+
+
+dir = firebase_init()
+
+
+  # get_request함수를 10초마다 한번씩 실행
+  # 공백이 반환되면 반복문의 처음으로 돌아가서 재실행
+  # 문자열이 반환되면 탈출
+while(True):
+    request_message = ''
+    print("readread")
+    request_message = get_request(dir)
+    time.sleep(3)
+
+    if request_message == 'Yet':
+        continue
+    else:
+        dir.child('message').set({'message' : request_message, 'status' : 'True'})
+        break
+
+print(request_message)
+  
+  # 파일에 메시지 저장
+f = open("a.txt", mode = 'wt', encoding = 'utf-8')
+f.write(request_message)
+f.close()
+
+  #save_record(dir, request_message, 'blue', '15', '20')
+
+
 isBlind = False #true = Blind state // #false = no blind state
 
 red = 13
@@ -31,15 +102,17 @@ pwmGreen.start(0)
 f = open("a.txt")
 
 line = f.readline()
+#f.write("")
+
 print(line)
 
-isWlqwnd = "집중" in line 								#case1
+isWlqwnd = "집중" in line	 							#case1
 isTodrkr = "생각" in line or "상상" in line or "아이디어" in line 			#case2
 isGbtlr = "슬퍼" in line or "우울" in line or "속상해" in line				#case3
-isDnsehd = "운동" in line								#case4
+isDnsehd = "운동" in line 								#case4
 isDkswjd = "불안" in line or "초조" in line or "짜증" in line or "쉬고" in line		#case5
 isDkarl = "암기" in line or "외우기" in line or "기억" in line 				#case6
-isEkdldjxm = "식욕" in line or "다이어트" in line or "살" in line or "빠져라" in line 	#case7
+isEkdldjxm = "식욕" in line or "다이어트" in line or "빠져라" in line		 	#case7
 
 if isWlqwnd:
     print("집중")
@@ -161,12 +234,8 @@ elif isEkdldjxm:
 
     os.system("python3 case7.py")
 
-
-
-
 else:
     print("no no no no")
-
 
 
 
