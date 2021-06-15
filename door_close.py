6bdd941d45f1610442b65c371b66742ee68cd6f1
whquddnr97 <59822853+whquddnr97@users.noreply.github.com>
2021-06-15 20:08:31 +0900

    Update door_close.py

--- door_close.py
+++ door_close.py
@@ -9,7 +9,7 @@ AIN2=18
 BIN2=22
 sig=deque([1,0,0,0])
 step=800
-dir=-1
+dir=1
 i = 0
 
 GPIO.setup(AIN1,GPIO.OUT,initial=GPIO.LOW)
@@ -18,7 +18,7 @@ GPIO.setup(AIN2,GPIO.OUT,initial=GPIO.LOW)
 GPIO.setup(BIN2,GPIO.OUT,initial=GPIO.LOW)
 
 try:
-    while i <= 2:
+    while i <= 4:
         for cnt in range(0,step):
             GPIO.output(AIN1,sig[0])
             GPIO.output(BIN1,sig[1])
